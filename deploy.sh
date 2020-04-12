a20d9361e783d2ec75bf076c19b467c206a4aa28
Yorrick Bakker <y.bakker@me.com>
2020-04-12 21:43:02 +0200

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Deploy to vectoric-s1
+HOST=docker-manager@s1.vectoric.nl
+BASE=$(basename $PWD)
+
+echo "Archiving to /tmp/$BASE.tar.gz..."
+cd ..
+tar --exclude="$BASE/node_modules" --exclude="$BASE/.git" -czvf /tmp/$BASE.tar.gz $BASE
+cd -
+
+echo "Copying to remote ($HOST)..."
+scp /tmp/$BASE.tar.gz $HOST:~/docker-projects/
+
+ssh -T $HOST "
+cd docker-projects
+echo 'unpacking archive...'
+tar -xzf $BASE.tar.gz && rm $BASE.tar.gz
+docker-compose -f $BASE/docker-compose.prod.yml up -d
+"
